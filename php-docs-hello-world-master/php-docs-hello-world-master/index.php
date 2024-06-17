2e5c59e23e9b1484db336410cd9d20d8dabdcc8a
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2024-06-17 20:49:16 +0530

    Update index.php

--- php-docs-hello-world-master/php-docs-hello-world-master/index.php
+++ php-docs-hello-world-master/php-docs-hello-world-master/index.php
@@ -1,4 +1,13 @@
 <?php
+// Basic self-introduction in PHP
 
-echo "Hello World!";
-echo "This id Ankit Repository";
+// Define variables for personal details
+$name = "Ankit Ranjan";
+$profession = "DevOps Engineer";
+$hobbies = ["reading", "coding", "hiking"];
+
+// Display the introduction
+echo "Hello, my name is $Ankit Ranjan.<br>";
+echo "I work as a $DEvOps Engineer.<br>";
+echo "My hobbies are " . implode($reading, "$Coding, $hobbies) . ".<br>";
+?>
