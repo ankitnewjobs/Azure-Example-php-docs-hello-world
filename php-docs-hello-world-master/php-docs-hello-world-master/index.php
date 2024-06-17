04f6f7478d92e4c8a775c730c632bb9f2348c578
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2024-06-17 20:53:18 +0530

    Update index.php

--- php-docs-hello-world-master/php-docs-hello-world-master/index.php
+++ php-docs-hello-world-master/php-docs-hello-world-master/index.php
@@ -1,13 +1,3 @@
 <?php
-// Basic self-introduction in PHP
 
-// Define variables for personal details
-$name = "Ankit Ranjan";
-$profession = "DevOps Engineer";
-$hobbies = ["reading", "coding", "hiking"];
-
-// Display the introduction
-echo "Hello, my name is $Ankit Ranjan.<br>";
-echo "I work as a $DEvOps Engineer.<br>";
-echo "My hobbies are " . implode($reading, "$Coding, $hobbies) . ".<br>";
-?>
+echo "Hello World!";
